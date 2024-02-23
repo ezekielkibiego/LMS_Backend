6cb6729faa9557fcc4267adcc8b685994c832494
ezekielkibiego <kibiezekiel@gmail.com>
2024-02-23 22:58:23 +0300

    add docker compose

--- src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
+++ src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
     // Update the DB_URL, DB_USER, and DB_PASSWORD to connect to the remote database
     private static final String DB_URL = "jdbc:postgresql://dpg-cnceouicn0vc73f1c8jg-a.oregon-postgres.render.com/lms_0k07";
-    private static final String DB_USER = "lms_0k07_user";
+    private static final String DB_USER = "lms_0k07_ezekielkibiego";
     private static final String DB_PASSWORD = "NoyxR2mDrrFD5E1JnOYjPDSAFxfirOjG";
 
     public static Connection getConnection() throws SQLException {
