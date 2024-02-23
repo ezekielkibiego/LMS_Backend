a89cab99d5ee3e0a97b3a44029ebf9a178aea804
ezekielkibiego <kibiezekiel@gmail.com>
2024-02-23 22:22:45 +0300

    add docker compose

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use the official OpenJDK image as the base image
+FROM openjdk:11-jre-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the compiled Java application JAR file into the container at /app
+COPY target/LMS-0.0.1-SNAPSHOT.jar /app
+
+# Expose the port your application runs on
+EXPOSE 8002
+
+# Command to run the application when the container starts
+CMD ["java", "-jar", "your-java-application.jar"]
--- src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
+++ src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
@@ -1,18 +1,16 @@
 package com.zeraki.co.ke.lms.db;
 
-// db/DatabaseManager.java
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/lms";
-    private static final String DB_USER = "kib";
-    private static final String DB_PASSWORD = "Kibiego22";
+    // Update the DB_URL, DB_USER, and DB_PASSWORD to connect to the remote database
+    private static final String DB_URL = "jdbc:postgresql://dpg-cnceouicn0vc73f1c8jg-a.oregon-postgres.render.com/lms_0k07";
+    private static final String DB_USER = "lms_0k07_user";
+    private static final String DB_PASSWORD = "NoyxR2mDrrFD5E1JnOYjPDSAFxfirOjG";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
-
 }
-
