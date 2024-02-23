52ea5355c5641a84030b2b2550dbf5069629eb37
ezekielkibiego <kibiezekiel@gmail.com>
2024-02-23 23:40:29 +0300

    modify db

--- src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
+++ src/main/java/com/zeraki/co/ke/lms/db/DatabaseManager.java
@@ -1,16 +1,21 @@
 package com.zeraki.co.ke.lms.db;
 
+// db/DatabaseManager.java
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseManager {
-    // Update the DB_URL, DB_USER, and DB_PASSWORD to connect to the remote database
-    private static final String DB_URL = "jdbc:postgresql://dpg-cnceouicn0vc73f1c8jg-a.oregon-postgres.render.com/lms_0k07";
-    private static final String DB_USER = "lms_0k07_ezekielkibiego";
-    private static final String DB_PASSWORD = "NoyxR2mDrrFD5E1JnOYjPDSAFxfirOjG";
+    private static final String DB_URL = "jdbc:postgresql://abul.db.elephantsql.com:5432/xnauyzyy";
+    private static final String DB_USER = "xnauyzyy";
+    private static final String DB_PASSWORD = "dTQMPutSbEiSLPlJ4DkLky0wAVTUIpTw";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
+
 }
+
+
+
